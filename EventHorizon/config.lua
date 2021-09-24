e2170e49f664b7bac073a2e542dbe8fb97fc6a63
Leylaa2 <42302732+Leylaa2@users.noreply.github.com>
2021-09-24 23:50:34 +0200

    Moved Folders

--- EventHorizon/config.lua
+++ EventHorizon/config.lua
@@ -17,45 +17,45 @@ local Warrior = class == "WARRIOR"
 
 --[[ EDIT BELOW THIS LINE ]]--
 
-config.showTrinketBars = false      -- Show bars for your equipped trinkets? Default = true
-config.castLine = true          -- Show a full-frame line at the end of casts/channels? Default = true (show for all casts). Setting this to a number will set the lower limit accordingly. ie, config.castLine = 0.5 will show all casts over 0.5 seconds. Setting this to 0 will show the line for all casts no matter what.
-config.gcdStyle = 'line'        -- 'line' = show a thin line for the GCD. 'bar' = show a full-frame shrinking bar for the gcd (looks good at a dark color and low opacity, set within the color table). false = disable GCD handling.
+config.showTrinketBars = false    -- Show bars for your equipped trinkets? Default = true
+config.castLine = true            -- Show a full-frame line at the end of casts/channels? Default = true (show for all casts). Setting this to a number will set the lower limit accordingly. ie, config.castLine = 0.5 will show all casts over 0.5 seconds. Setting this to 0 will show the line for all casts no matter what.
+config.gcdStyle = 'line'          -- 'line' = show a thin line for the GCD. 'bar' = show a full-frame shrinking bar for the gcd (looks good at a dark color and low opacity, set within the color table). false = disable GCD handling.
 
-config.enableRedshift = false      -- Enable Redshift's show/hide functions? Default = false
-config.Redshift.showCombat = true     -- Always show EH in combat. Default = true
-config.Redshift.showHarm = true      -- Show EH if targeting an attackable unit, in or out of combat. Default = true
+config.enableRedshift = false       -- Enable Redshift's show/hide functions? Default = false
+config.Redshift.showCombat = true   -- Always show EH in combat. Default = true
+config.Redshift.showHarm = true     -- Show EH if targeting an attackable unit, in or out of combat. Default = true
 config.Redshift.showHelp = false    -- Show EH if targeting a friendly (unattackable) unit, in or out of combat. Default = false
-config.Redshift.showBoss = true      -- Always show EH when targeting a boss-level unit, regardless of its attackable state. Default = true
-config.Redshift.showFocus = false    -- Always show EH while a focus unit exists, in or out of combat. Default = false
-config.Redshift.hideVehicle = true    -- Hide EH if using a vehicle with an actionbar of its own. Does not affect vehicles that do not change the actionbar. If you use Vitals, you may want to set this to false. Default = true
-config.Redshift.hideVitals = true    -- Hide EventHorizon_Vitals as well when the main frame is hidden. Default = true
+config.Redshift.showBoss = true     -- Always show EH when targeting a boss-level unit, regardless of its attackable state. Default = true
+config.Redshift.showFocus = false   -- Always show EH while a focus unit exists, in or out of combat. Default = false
+config.Redshift.hideVehicle = true  -- Hide EH if using a vehicle with an actionbar of its own. Does not affect vehicles that do not change the actionbar. If you use Vitals, you may want to set this to false. Default = true
+config.Redshift.hideVitals = true   -- Hide EventHorizon_Vitals as well when the main frame is hidden. Default = true
 
-config.Lines = nil            -- This draws extra marker lines on EventHorizon. Examples: config.Lines = 2 (add a line at 2 seconds in the future); config.Lines = {2, 4, 6} (add lines at 2, 4 and 6 seconds). Decimals and math are permitted, of course. Any amount of lines is allowed. Default: Disabled.
-config.LinesColor = {1,1,1,0.5}      -- Standard color syntax {R,G,B,A} (see below). No classcolor allowed quite yet. Embed more tables for more colors. {{1,0,0,1},{0,1,0,1},{0,0.5,1}} will do one red line, one green line, and the rest kinda-teal lines. Yeah, it's a bit of an advanced thing.
+config.Lines = nil                  -- This draws extra marker lines on EventHorizon. Examples: config.Lines = 2 (add a line at 2 seconds in the future); config.Lines = {2, 4, 6} (add lines at 2, 4 and 6 seconds). Decimals and math are permitted, of course. Any amount of lines is allowed. Default: Disabled.
+config.LinesColor = {1,1,1,0.5}     -- Standard color syntax {R,G,B,A} (see below). No classcolor allowed quite yet. Embed more tables for more colors. {{1,0,0,1},{0,1,0,1},{0,0.5,1}} will do one red line, one green line, and the rest kinda-teal lines. Yeah, it's a bit of an advanced thing.
 
 config.anchor = {"TOPLEFT", "EventHorizonHandle", "BOTTOMLEFT"}  -- EventHorizon's position. Format: {'POINT', 'AnchorFrame', 'RELATIVE', Xoffset (optional), Yoffset (optional)}. Set the anchor to something other than EventHorizonHandle to root EH in place and remove the anchor. Default = {"TOPRIGHT", "EventHorizonHandle", "BOTTOMRIGHT"} (anchor the TOPRIGHT of EH to EventHorizonHandle's BOTTOMRIGHT with no offset)
 config.width = 375            -- Width of a single bar, not counting its icon. The actual width of the frame is (width+height+[padding*2]).
 config.height = 20            -- Height of a single bar. If you use config.staticHeight, this is used for the width of the bar icons.
 config.spacing = 0            -- Vertical spacing between bars.
-config.scale = 1            -- The overall scale of the main frame.
-config.staticheight = 150        -- Changing this to a number will override config.height and tell EH to resize the bars instead of the frame. For example, 'config.staticheight = 150' will make EH 150 pixels tall. With 5 shown bars, each bar will be 30 pixels tall. Spacing is still used in this mode and will act the same as it does otherwise.
-config.staticframes = 7          -- When used with config.staticheight, sets a minimum number of bars to use static height settings with. For example, with staticframes = 4 and 3 bars shown, EventHorizon will use normal variable height settings.
-config.hideIcons = false        -- Hides the bar icons completely. This will make the frame a little more narrow, as config.height will no longer add to its width.
+config.scale = 1              -- The overall scale of the main frame.
+config.staticheight = 150     -- Changing this to a number will override config.height and tell EH to resize the bars instead of the frame. For example, 'config.staticheight = 150' will make EH 150 pixels tall. With 5 shown bars, each bar will be 30 pixels tall. Spacing is still used in this mode and will act the same as it does otherwise.
+config.staticframes = 7       -- When used with config.staticheight, sets a minimum number of bars to use static height settings with. For example, with staticframes = 4 and 3 bars shown, EventHorizon will use normal variable height settings.
+config.hideIcons = false      -- Hides the bar icons completely. This will make the frame a little more narrow, as config.height will no longer add to its width.
 
 config.past = -1.5            -- Number of seconds to show in the past. Default = -3 (yes, it's a negative number)
 config.future = 12            -- Number of seconds to show in the future. Recommended to keep this between 12-18 unless you've got some crazy configuration. Default = 12
 
-config.texturedbars = true        -- You have the choice between textures and solid colors. Remove this or comment it out to see what the solids look like. Default = true
+config.texturedbars = true         -- You have the choice between textures and solid colors. Remove this or comment it out to see what the solids look like. Default = true
 config.bartexture = "Interface\\Addons\\EventHorizon\\Smooth"  -- Full path, minus extension, of the bar texture to use. Default = "Interface\\Addons\\EventHorizon\\Smooth"
-config.texturealphamultiplier = 2    -- Most textures appear darker than a solid color because of the alpha value. To counteract this, the opacity of the color is multiplied by this number before it's applied to a texture.
-config.barbg = false          -- Shows a backdrop behind each individual bar. The color of the backdrop may be set within the color table below.
+config.texturealphamultiplier = 2  -- Most textures appear darker than a solid color because of the alpha value. To counteract this, the opacity of the color is multiplied by this number before it's applied to a texture.
+config.barbg = false               -- Shows a backdrop behind each individual bar. The color of the backdrop may be set within the color table below.
 
-config.backdrop = true                    -- Use the background and border frame? Default = true
-config.padding = 3                      -- Extra space between the bar edges and the backdrop/border.
-config.bg = "Interface\\ChatFrame\\ChatFrameBackground"    -- Frame background texture. Full path, no extension. Default = "Interface\\ChatFrame\\ChatFrameBackground"
+config.backdrop = true           -- Use the background and border frame? Default = true
+config.padding = 3               -- Extra space between the bar edges and the backdrop/border.
+config.bg = "Interface\\ChatFrame\\ChatFrameBackground"   -- Frame background texture. Full path, no extension. Default = "Interface\\ChatFrame\\ChatFrameBackground"
 config.border = "Interface\\Tooltips\\UI-Tooltip-Border"  -- Border texture, same thing all over. Default = "Interface\\Tooltips\\UI-Tooltip-Border" 
-config.edgesize = 8                      -- Thickness of the frame's border. Default = 8
-config.inset = {top = 2, bottom = 2, left = 2, right = 2}  -- Backdrop insets move the edges of the background, but leave the border untouched. Long story short, start at 1/4 your edgesize and adjust by 1 until it looks right. Default = {top = 2, bottom = 2, left = 2, right = 2}
+config.edgesize = 8              -- Thickness of the frame's border. Default = 8
+config.inset = {top = 2, bottom = 2, left = 2, right = 2} -- Backdrop insets move the edges of the background, but leave the border untouched. Long story short, start at 1/4 your edgesize and adjust by 1 until it looks right. Default = {top = 2, bottom = 2, left = 2, right = 2}
 
 
 config.stackFont = "fonts\\arialn.ttf"          -- Changes the font of the stack indicators. Usage: config.stackFont = "Interface\\AddOns\\FontDirectory\\FontName.ttf"
@@ -69,21 +69,21 @@ config.stackOnRight = false                     -- Changes the position of the s
 -- The format for the color section is {Red, Green, Blue, Opacity/Alpha}. To color a bar or indicator by class, use {true, <burn>, <alpha>}.
 -- <burn> means color intensity - 0.5 burn darkens a color by 50%. 1.2 lightens it by 20%. 1 is baseline.
 local c = EventHorizon.colors
-c.sent = {true,Priest and 0.7 or 1,0.5}      -- Marker line when a spellcast is sent to the server. Default = {true,Priest and 0.7 or 1,0.5} (class colored, dimmed a bit if you're a Priest, 50% opacity)
-c.tick = {true,Priest and 0.7 or 1,1}      -- Tick markers. Default = {true,Priest and 0.7 or 1,1} (class colored, dimmed a bit if you're a Priest, opaque)
+c.sent = {true,Priest and 0.7 or 1,0.5} -- Marker line when a spellcast is sent to the server. Default = {true,Priest and 0.7 or 1,0.5} (class colored, dimmed a bit if you're a Priest, 50% opacity)
+c.tick = {true,Priest and 0.7 or 1,1}   -- Tick markers. Default = {true,Priest and 0.7 or 1,1} (class colored, dimmed a bit if you're a Priest, opaque)
 c.channeltick = {0,1,0.2,0.25}          -- Tick markers for channeled spells. Default is the same as casting.
 c.casting = {0,1,0.2,0.25}              -- Casting bars. Default = {0,1,0,0.25} (green, 0.25 unmodified alpha)
-c.castLine = {0,1,0,0.3}            -- The end-of-cast line, shown for casts and channels over 1.5 seconds. Default = {0,1,0,0.3} (green, 0.3 unmodified alpha)
+c.castLine = {0,1,0,0.3}                -- The end-of-cast line, shown for casts and channels over 1.5 seconds. Default = {0,1,0,0.3} (green, 0.3 unmodified alpha)
 c.cooldown = {0.6,0.8,1,0.3}            -- Cooldown bars. Default = {0.6,0.8,1,0.3} (mute teal, 0.3 unmodified alpha)
 c.recharge = {0.6,0.8,1,0.3}            -- Recharge bars. Default = {0.6,0.8,1,0.3} (mute teal, 0.3 unmodified alpha)
 c.debuffmine = {true,Priest and 0.7 or 1,0.3}  -- YOUR debuff bars. Default = {true,Priest and 0.7 or 1,0.3} (class colored, dimmed a bit if you're a Priest, 0.3 unmodified alpha)
-c.debuff = {true,0.5,0.3}            -- OTHER PLAYERS' debuff bars. Default = {true,0.5,0.3} (class colored, darkened by 50%, 0.3 unmodified alpha)
+c.debuff = {true,0.5,0.3}               -- OTHER PLAYERS' debuff bars. Default = {true,0.5,0.3} (class colored, darkened by 50%, 0.3 unmodified alpha)
 c.playerbuff = {true,Priest and 0.7 or 1,0.3}  -- Buff bars. Default = {true,Priest and 0.7 or 1,0.3} (class colored, dimmed a bit if you're a Priest, 0.3 unmodified alpha)
-c.nowLine = {1,1,1,0.3}              -- The "Now" line.
-c.bgcolor = {0,0,0,0.6}              -- Color of the frame's background. Default = {0,0,0,0.6} (black, 60% opacity)
-c.bordercolor = {1,1,1,1}            -- Color of the frame's border. Default = {1,1,1,1} (white, fully opaque)
-c.gcdColor = {1,1,1,0.5}            -- Color of the GCD indicator. Default = {1,1,1,0.5}
-c.barbgcolor = {1,1,1,0.1}            -- Color of bar backgrounds. Default = {1,1,1,0.1} (barely visible)
+c.nowLine = {1,1,1,0.3}                 -- The "Now" line.
+c.bgcolor = {0,0,0,0.6}                 -- Color of the frame's background. Default = {0,0,0,0.6} (black, 60% opacity)
+c.bordercolor = {1,1,1,1}               -- Color of the frame's border. Default = {1,1,1,1} (white, fully opaque)
+c.gcdColor = {1,1,1,0.5}                -- Color of the GCD indicator. Default = {1,1,1,0.5}
+c.barbgcolor = {1,1,1,0.1}              -- Color of bar backgrounds. Default = {1,1,1,0.1} (barely visible)
 
 --[[ This table adjusts blending modes for most portions of EventHorizon. These are not normally usable on Statusbar objects, which EventHorizon is fairly unique about not using.
 From wowprogramming.com's descriptions (which are very apt):
@@ -122,7 +122,7 @@ EventHorizon.layouts = {    -- You can actually add anything from the table abov
     top = 0,
     bottom = 0.12,
   },
-  smalldebuff = {        -- The recast line for spells like Vampiric Touch and Immolate.
+  smalldebuff = {     -- The recast line for spells like Vampiric Touch and Immolate.
     top = 0,
     bottom = 0.25,
   },
@@ -134,7 +134,7 @@ EventHorizon.layouts = {    -- You can actually add anything from the table abov
     top = 0.5,
     bottom = 1,
   },
-  default = {          -- Just about everything else.
+  default = {         -- Just about everything else.
     top = 0,
     bottom = 1,
   },
---
+++ EventHorizon_Configs/paladin_my_config.lua
@@ -0,0 +1,390 @@
+-- 2017.06.28
+local usemouseover = true -- Make this false or nil (or just delete the line altogether) to make your healing bars not change when you mouse over something.
+
+function EventHorizon:InitializeClass()
+  self.config.gcdSpellID = 19750 -- Flash of Light
+  self.config.hastedSpellID = {7328,10} -- Redemption, probably not needed at all
+
+
+  -- ---------------------------------------
+  -- Holy
+  -- ---------------------------------------
+
+  -- Crusader Strike
+   self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 3,
+    cooldown = 35395,
+  })
+  
+  -- Judgment
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 18,
+    cooldown = 20271,
+    debuff = 196941,
+  })
+
+  -- Holy Shock
+  self:newSpell({
+    requiredTree = 1,
+    requiredLevel = 10,
+    cooldown = 20473,
+	keepIcon = true,
+    cast = {20473, 19750, 82326},
+  })
+
+  -- Light of Dawn
+  self:newSpell({
+    requiredTree = 1,
+    requiredLevel = 40,
+    cooldown = 85222,
+  })
+
+  -- Tyr's Deliverance
+  self:newSpell({
+    requiredTree = 1,
+    requiredArtifactTalent = 200652,
+    cooldown = 200652,
+    playerbuff = 200652,
+    auraunit = usemouseover and 'mouseover' or 'target',
+  })
+ 
+  -- Bestow Faith
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 1,
+    cooldown = 223306,
+  })
+
+  -- Beacon of Virtue
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 21,
+    cooldown = 200025,
+    playerbuff = 200025,
+    auraunit = usemouseover and 'mouseover' or 'target',
+  })
+  
+  -- Holy Prism
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 15,
+    cooldown = 114165,
+  })
+ 
+  -- Light's Hammer
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 2,
+    cooldown = 114158,
+  })
+
+  -- Holy Avenger
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 14,
+    cooldown = 105809,
+    playerbuff = 105809,
+  })
+  
+  -- Avenging Wrath
+  self:newSpell({
+    requiredTree = 1,
+	requiredLevel = 80,
+    cooldown = 31842,
+    playerbuff = 31842,
+  })
+  
+  -- Aura Mastery
+  self:newSpell({
+    requiredTree = 1,
+    requiredLevel = 70,
+    cooldown = 31821,
+    playerbuff = 31821,
+  })
+    
+  -- Rule of Law
+  self:newSpell({
+    requiredTree = 1,
+    requiredTalent = 6,
+	playerbuff = 214202,
+    recharge = 214202,
+  })
+  
+ 
+  -- ---------------------------------------
+  -- Protection
+  -- ---------------------------------------
+
+
+  -- Hammer of the Righteous
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 10,
+	requiredTalentUnselected = 2,
+	recharge = 53595,
+  })
+  
+  -- Blessed Hammer
+  self:newSpell({
+    requiredTree = 2,
+	requiredTalent = 2,
+    -- debuff = 204301,
+	recharge = 204019,
+  })
+
+    -- Judgment
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 3,
+    --cooldown = 20271,
+	recharge = 20271,
+  })
+
+  -- Consecration
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 14,
+    cooldown = 26573,
+    --debuff = {26573,1},
+  })
+  
+  -- Avenger's Shield
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 10,
+    cooldown = 31935,
+  })
+  
+  -- Shield of the Righteous
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 20,
+	playerbuff = 53600,
+    recharge = 53600,
+  })
+
+    -- Eye of Tyr
+  self:newSpell({
+    requiredTree = 2,
+	requiredArtifactTalent = 209202,
+    cooldown = 209202,
+    debuff = 209202,
+  })
+
+    -- Light of the Protector
+  self:newSpell({
+    requiredTree = 2,
+    requiredLevel = 40,
+	requiredTalentUnselected = 13,
+    cooldown = 184092,
+  })
+  
+  -- Hand of the Protector
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 13,
+    cooldown = 213652,
+  })
+  
+--[[
+  -- Knight Templar
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 14,
+    playerbuff = 221886,
+	recharge = 190784,
+  })
+]]--
+
+    -- Seraphim
+  self:newSpell({
+    requiredTree = 2,
+    requiredTalent = 20,
+    cooldown = 152262,
+    playerbuff = 152262,
+  })
+
+  -- Avenging Wrath
+  self:newSpell({
+    requiredTree = 2,
+	requiredLevel = 80,
+    cooldown = 31884,
+    playerbuff = 31884,
+  })
+
+  -- Bastion of Light
+  self:newSpell({
+	requiredTree = 2,
+    requiredTalent = 5,
+    cooldown = 204035,
+  })
+
+--[[
+  -- Blessing of Spellwarding
+  self:newSpell({
+    requiredTree = 2,
+	requiredTalent = 10,
+    cooldown = 204018,
+    playerbuff = 204018,
+    auraunit = usemouseover and 'mouseover' or 'player',
+  })
+]]--
+
+--[[
+  -- Aegis of Light
+  self:newSpell({
+    requiredTree = 2,
+	requiredTalent = 16,
+    cooldown = 204150,
+    playerbuff = 204150,
+    auraunit = usemouseover and 'mouseover' or 'player',
+  })
+]]--
+
+
+  -- ---------------------------------------
+  -- Retribution
+  -- ---------------------------------------
+
+  -- Judgment
+  self:newSpell({
+    requiredTree = 3,
+    requiredLevel = 3,
+    cooldown = 20271,
+    debuff = {197277,0},
+  })
+
+  -- Blade of Justice
+  self:newSpell({
+    requiredTree = 3,
+    requiredLevel = 20,
+    requiredTalentUnselected = 12,
+    cooldown = 184575,
+  })
+
+  -- Divine Hammer
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 12,
+    cooldown = 198034,
+    debuff = {198034,2},
+  })
+
+  -- Wake of Ashes
+  self:newSpell({
+    requiredTree = 3,
+    requiredArtifactTalent = 205273,
+    cooldown = 205273,
+    debuff = 205273,
+  })
+  
+  -- Execution Sentence
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 2,
+    cooldown = 213757,
+    debuff = {213757,7},
+    --hasted = true,
+  })
+ 
+    -- Crusader Strike
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalentUnselected = 5,
+    recharge = 35395,
+  })
+
+  -- Zeal
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 5,
+    recharge = 217020,
+    --playerbuff = 217020,
+  })
+
+  -- Consecration
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 3,
+    cooldown = 205228,
+  })
+
+  -- Holy Wrath
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 21,
+    cooldown = 210220,
+  })
+
+  -- Avenging Wrath
+  self:newSpell({
+    requiredTree = 3,
+	requiredLevel = 80,
+    requiredTalentUnselected = 20,
+    cooldown = 31884,
+    playerbuff = 31884,
+  })
+
+  -- Crusade
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 20,
+    cooldown = 231895,
+    playerbuff = 231895,
+  })
+
+
+  -- Retribution Survivability Abilities and Talents
+
+
+  -- Divine Shield
+  self:newSpell({
+    requiredTree = 3,
+    requiredLevel = 18,
+    cooldown = 642,
+    playerbuff = 642,
+  })
+
+--[[
+  -- Shield of Vengeance
+  self:newSpell({
+    requiredTree = 3,
+    cooldown = 184662,
+    playerbuff = 184662,
+  })
+]]--
+
+--[[
+  -- Eye for an Eye
+  self:newSpell({
+    requiredTree = 3,
+    requiredTalent = 14,
+    cooldown = 205191,
+    playerbuff = 205191,
+  })
+]]--
+
+
+
+  -- Situational CC spells, not included by default.
+--[[
+  -- Repentance
+  self:newSpell({
+    requiredTree = 1,2,3,
+    requiredTalent = 8,
+    cooldown = 20066,
+  })
+]]--
+
+--[[
+  -- Blinding Light
+  self:newSpell({
+    requiredTree = 1,2,3,
+    requiredTalent = 9,
+    cooldown = 115750,
+  })
+]]--
+
+end
